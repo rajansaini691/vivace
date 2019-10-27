38a6c174b7afab619d98fc34552e55c90314835a
Rajan <rajansaini691@gmail.com>
2019-10-27 01:31:29 -0700

    Made wording clearer

--- src/processing/jack.py
+++ src/processing/jack.py
@@ -1,9 +1,10 @@
 """
-Starts a jack client. This is one of the ways we can pass input to the main
-audio processor. We'll debug this by rendering a simple FFT in-house, but the 
-data should be passed over to the main processor. The safest way to do this is
-to update an array every process callback, which the processor can then read
-from. This keeps the audio reads independent from the output time. A possible
-problem with this approach is that there is no way to maintain a history for
-Fourier analysis. Perhaps we can store a buffer of multiple frames? 
+Starts a jack client
+
+ - Passes audio playing from system programs to the main audio processor
+ - We'll test this by rendering a simple FFT in-house
+ - Updates a sample array every audio frame
+   - Keeps the audio reads independent from the time to process the data and
+     write to the pixel map
+   - Should be a buffer of multiple frames so we can analyze history
 """
