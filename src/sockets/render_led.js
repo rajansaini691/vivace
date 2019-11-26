9407e455b445bf62596c90f51207128880954f7b
Rajan <rajansaini691@gmail.com>
2019-11-25 22:25:55 -0800

    Refactor client code  - Separate scripting from HTML  - Set up simple rendering

--- src/sockets/render_led.js
+++ src/sockets/render_led.js
@@ -1,41 +1,47 @@
-window.onload = function() {
-	// Opens a web socket at port 4444
+function render() {
+	// Init
+	var c = document.getElementById("led_renderer");
+	var ctx = c.getContext("2d");
+
+	// Draw a single LED
+	ctx.beginPath();
+	ctx.rect(20, 20, 150, 100);
+	ctx.stroke();
+}
+
+function WebSocketTest() {
+	// Opens a websocket
 	var ws_addr = "ws://" + location.hostname + ":4444";
 	var ws = new WebSocket(ws_addr);
-	console.log("TEST");
 
-	/*
-	 * Called whenever the server pushes data
-	 */
 	ws.onmessage = function (evt) { 
-		// Stores the rgb array being rendered
-		var pixels = [];
-
-		// Stores initial binary data coming from server
 		var arr = evt.data;
 
-		/*
-		 * Convert socket data to an array
-		 */
-		const fr = new FileReader();
-		fr.readAsArrayBuffer(arr);
-
-		// The incoming data format
+		// TODO Get this value from configuration
 		var BYTES_PER_INT = 1;
 
+		// Convert bytes to int array
+		const fr = new FileReader();
+		fr.readAsArrayBuffer(arr);
 		fr.onload = () => {
-			const data = new Uint8Array(fr.result);
+		const data = new Uint8Array(fr.result);
 
-			for(var i = 0; i < data.length / BYTES_PER_INT; i += 1) {
-				pixels.push(0);
-
-				for(var j = 0; j < BYTES_PER_INT; j++) {
-					pixels[i] <<= 8;
-					pixels[i] |= data[i * BYTES_PER_INT + j];
-				}
+		var pixels = [];
+		for(var i = 0; i < data.length / BYTES_PER_INT; i += 1) {
+			pixels.push(0);
+			for(var j = 0; j < BYTES_PER_INT; j++) {
+				pixels[i] <<= 8;
+				pixels[i] |= data[i * BYTES_PER_INT + j];
 			}
+			
 		}
-		console.log("TEST");
 		console.log(pixels);
+		}
+	};
+	
+	ws.onclose = function() { 
+	  // websocket is closed.
+	  alert("Connection is closed..."); 
 	};
 }
+
