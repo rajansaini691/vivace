0568ed0e07b4c1087be0c768c0de246a97f1184c
Rajan <rajansaini691@gmail.com>
2020-03-07 00:46:22 -0800

    Change num pixels to 150 to match hardware

--- src/rendering/rendering_conf.py
+++ src/rendering/rendering_conf.py
@@ -2,7 +2,7 @@
 Stores all of the relevant hardware configuration
 """
 # Number of RGB LEDs
-NUM_PIXELS = 200
+NUM_PIXELS = 50
 
 # Number of seconds between writes to the LED strip
 WRITE_PERIOD = 0.05
