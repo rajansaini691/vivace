cfeae5890c7426f9c1d69b5eaea7329db3929383
its20pastday <gjlee@ucsb.edu>
2020-03-13 19:12:22 -0700

    Switches center to 5/12

--- src/rendering/overlay.py
+++ src/rendering/overlay.py
@@ -27,7 +27,7 @@ def draw_overlay(pixel_map, color, strength):
     overlay_radius = 8
 
     # Halfway mark
-    halfway_mark = len(pixel_map) / 3
+    halfway_mark = 5 * len(pixel_map) / 12
 
     # Number of pixels away from the center
     dist = halfway_mark * strength
