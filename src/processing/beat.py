a6d6f5a86ad21c0bdcc8f149127620bc184c97cf
Rajan <rajansaini691@gmail.com>
2020-03-14 01:45:46 -0700

    Beat detector compiles [UNTESTED])

--- src/processing/beat.py
+++ src/processing/beat.py
@@ -1,7 +1,41 @@
-import essentia
+from essentia.standard import SingleBeatLoudness
+import numpy as np
 
 
-def compute_beat(audio_buffer):
+class BeatDetector:
     """
-    Test of Essentia library (should refactor once working)
+    Computes presence of a beat
     """
+    def __init__(self, buffer_size, fs, window_frames, beat_frames):
+        """
+        Parameters:
+            buffer_size     Number of samples in each frame (power of 2, like
+                            1024)
+            fs              Samplerate (usually 44100)
+            window_frames   Number of frames per beat window (usually small;
+                            library default is 5)
+            beat_frames     Number of frames to compute the energy (smaller
+                            than window_frames, library default is 2)
+        """
+        # Constants
+        self.BUFFER_SIZE = buffer_size
+
+        # The Essentia algorithm
+        beat_duration = beat_frames * buffer_size / fs
+        window_duration = window_frames * buffer_size / fs
+        self.beat_alg = SingleBeatLoudness(
+                beatDuration=beat_duration,
+                beatWindowDuration=window_duration
+        )
+
+        # Stores previous values of the audio buffer for higher accuracy
+        self.buffer = buffer_size * (window_frames + beat_frames) * [0]
+
+    def process(self, audio_buffer):
+        """
+        Test of Essentia library (should refactor once working)
+        """
+        self.buffer = self.buffer[self.BUFFER_SIZE:] + audio_buffer
+
+        # Run algorithm
+        return self.beat_alg(self.buffer)[0]
