2e9652d22810b7678ce425b60eab37b947f19251
Rajan <rajansaini691@gmail.com>
2020-03-16 00:22:41 -0700

    Fix slider bug

--- src/rendering/rendering_conf.py
+++ src/rendering/rendering_conf.py
@@ -6,3 +6,7 @@ NUM_PIXELS = 450
 
 # Number of seconds between writes to the LED strip
 MAX_WRITE_PERIOD = 0.03
+
+# Index of the pixel at the "center" of the strip (dependent on how the LEDs
+# are set up)
+CENTER = int(NUM_PIXELS * 23 / 60)
--- src/rendering/sliders.py
+++ src/rendering/sliders.py
@@ -5,26 +5,29 @@ from rendering.overlay import combine_colors
 # TODO Find a better, more descriptive name
 class Slider:
     def __init__(self):
-        # Stores the previous values
-        self.buffer = int(rendering_conf.NUM_PIXELS / 2) * [(0, 0, 0)]
+        # Length of buffer we must store
+        buffer_len = max(rendering_conf.CENTER,
+                         rendering_conf.NUM_PIXELS - rendering_conf.CENTER)
+
+        # Stores previous slider values (a copy of half of the pixel map)
+        self.buffer = buffer_len * [(0, 0, 0)]
 
         # Frames between updates
         self.FRAME_DELAY = 2
         self.frames = 0
 
     def update(self, pixel_map, strength, color):
-        halfway = 23 * len(pixel_map) / 60
-        extreme = 23 * (len(pixel_map) - 1) / 60    # Better name
         for i, pixel in enumerate(pixel_map):
-            buf_index = int(halfway * (1 - abs(i - extreme) / (extreme)))
+            # Indexes the buffer
+            buf_index = abs(rendering_conf.CENTER - i)
 
             pixel_map[i] = combine_colors(
                 strength,
-                pixel_map[i],
+                pixel,
                 self.buffer[buf_index]
             )
 
-        if strength < 0.4:
+        if strength < 0.2:
             color = (0, 0, 0)
 
         # Update speed
@@ -33,4 +36,5 @@ class Slider:
 
         if self.frames == 0:
             # Move sliders over
-            self.buffer = self.buffer[1:] + [color]
+            self.buffer.insert(0, color)
+            self.buffer.pop()
